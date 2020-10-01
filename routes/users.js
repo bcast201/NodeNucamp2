cdf70c3743d721d8cc32fa216192f788e6b4ba4d
bcast201 <bcast201@gmail.com>
2020-10-01 11:19:55 -0500

    Assignment 3

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,15 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-    res.send('respond with a resource');
+router.get('/',  (req, res, next) => {
+  User.find()
+  .then(users => {
+    console.log('users :', users)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(users); []
+  })
+    .catch(err => next(err));
 });
 
 router.post('/signup', (req, res) => {
